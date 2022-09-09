7aa1f5dee4ca5fdc3ea3fadad9eca764ff0dc348
Ddark91 <aymenmo1991@gmail.com>
2022-09-08 19:46:42 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print the string
+ *
+ * description:using the main function
+ * prints
+ * return (0)
+ */
+int main(void)
+{
+     char c;
+int i;
+long li;
+long long lli;
+float f;
+printf("size of a char is;:%1d byte(s)\n", sizeof(c));
+printf("size of int:%1d byte(s)\n", sizeof(i));
+printf("size of long is:%1d byte(s)\n", sizeof(li));
+printf("size of long long is:%1d byte(s)\n", sizeof(lli));
+printf("size of float is:%1d byte(s)\n", sizeof(f));
+return (0);
+}
+
+
