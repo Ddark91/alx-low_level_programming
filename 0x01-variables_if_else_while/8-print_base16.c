7f0d582274f3d7e71eed6e8499fea8fc73a1889f
Ddark91 <aymenmo1991@gmail.com>
2022-09-11 23:40:13 -0700

    print2

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int d;
+	char low;
+
+	for (d = 'a'; d<= '0'; d++)
+		putchar(d);
+	for(low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
