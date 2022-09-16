69521e87b6816493cd2e6fd27cdfa9bc49416f52
Ddark91 <aymenmo1991@gmail.com>
2022-09-15 19:20:39 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_lines(int n)
+{
+	int co;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= n; co++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
+
