b728b7c7f6c63a3359b52c85c6d8261efb073a85
Ddark91 <aymenmo1991@gmail.com>
2022-09-18 06:56:47 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,7 @@
 /**
  * main - print if the number is postive, zero or negative
  *
- * Description: using the main function
- * this program prints"programming is postive,zero or negative"
- * Retutrn: 0
+ * Return: 0
  */
 int main(void)
 {
@@ -25,18 +23,3 @@ int main(void)
 
 	return (0);
 }
-/*
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
-	return (0);
-}
-*/
